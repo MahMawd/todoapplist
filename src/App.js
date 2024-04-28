75a7f512ef249bee1dae186a0d2ee36f4b94112f
MahMawd <mahmoudbenmoussa91@gmail.com>
2024-04-28 20:04:08 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 
 import './App.css';
-import { TodoWrapper } from './components/TodoWrapper';
+import { Navbar } from './components/Navbar';
+//import { TodoWrapper } from './components/TodoWrapper';
 import { TodoWrapperLocalStorage } from './components/TodoWrapperLocalStorage';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
     <TodoWrapperLocalStorage />
+    
     </div>
+    
   );
 }
 
