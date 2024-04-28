11d0e54be7967901d3014a3150a2be7a6435f7ce
MahMawd <mahmoudbenmoussa91@gmail.com>
2024-04-28 21:18:37 +0100

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Navbar } from './components/Navbar';
-//import { TodoWrapper } from './components/TodoWrapper';
+import { AboutUs } from './components/AboutUs';
 import { TodoWrapperLocalStorage } from './components/TodoWrapperLocalStorage';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    <TodoWrapperLocalStorage />
-    
-    </div>
-    
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="main">
+          <Routes>
+            <Route path="/" element={<TodoWrapperLocalStorage />} />
+            <Route path="/about" element={<AboutUs />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
